3197d0a266e4f77893e643ec61730c84991bd5c4
Ho-Ling Poon <holingpoon@nypl.org>
2017-09-18 12:08:47 -0400

    REQ-254 Scaffolding listener.js listener.php CancelRequestResultConsumerListener class.

---
+++ src/CancelRequestResultConsumerListener.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NYPL\CancelRequestResultConsumer;
+
+use NYPL\Starter\Listener\Listener;
+
+class CancelRequestResultConsumerListener extends Listener
+{
+    protected function processListenerEvents()
+    {
+        // TODO: Implement processListenerEvents() method.
+    }
+}
